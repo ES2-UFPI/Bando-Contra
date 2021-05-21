04266debd76444e58cf4ef1a0b4522aeba56c8eb
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-05-21 17:37:22 -0300

    Add detailClient view #7

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,13 +1,22 @@
 from django.contrib.auth.models import User
 from .utils import ModelField
+from .utils import UserFacade
+from abc import abstractmethod
 
-# Create your models here.
+@abstractmethod
+def getTemplatesLocation(self):
+    pass
+UserFacade.addMethodToUser('getTemplatesLocation', getTemplatesLocation)
+
 class ClientUser (User):
     fieldCreator = ModelField
     cpf = fieldCreator.createCpf()
     address = fieldCreator.createAddress()
     phone = fieldCreator.createPhone()
     bornDate = fieldCreator.createDate("Born Date")
+
+    def getTemplatesLocation(self):
+        return "core/user/client/"
 
     def __str__ (self):
         return self.cpf
--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 class ModelField:
     def createAddress ():
@@ -15,3 +17,33 @@ class ModelField:
         phone_regex = RegexValidator(regex=r'\(\d{2}\)\d{4,5}-\d{4}', message="Informe um telefone válido. Exemplos: (99)99999-9999; (99)9999-9999")
         return models.CharField("Phone Number", max_length = 18, default = None, validators = [phone_regex])
 
+class ShortcutsFacade:
+    @staticmethod
+    def callRender(request, template, data={}):
+        return render(request, template, data)
+
+class UserFacade:
+    @staticmethod
+    def addMethodToUser(methodName,method):
+        User.add_to_class(methodName, method)
+
+    @staticmethod
+    def getUser(userModel, username):
+        return get_object_or_404(userModel, username=username)
+
+class UserContext:
+    def __init__(self, user):
+        self._user = user
+
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, user):
+        self._user = user
+    
+    def detailView(self, request):
+        result = self._user.getTemplatesLocation() + "detail.html"
+        data = {'user': self._user}
+        return ShortcutsFacade.callRender(request, result, data)
