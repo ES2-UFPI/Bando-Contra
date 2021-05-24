e03bd42ce849241990120baa1391a6c4009c0f9b
Francisco Jair <fjair123@ufpi.edu.br>
2021-05-24 09:07:10 -0300

    creation of the visualization logic of the partner type user

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import RegexValidator
-
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,13 @@
-from .models import ClientUser
+from .models import ClientUser, PartnerUser
 from .utils import UserContext
-from .utils import ShortcutsFacade
 from .utils import UserFacade
 
 def detailClient(request):
     user = UserFacade.getUser(ClientUser, request.user.username)
+    context = UserContext(user)
+    return context.detailView(request)
+
+def detailPartner(request):
+    user = UserFacade.getUser(PartnerUser, request.user.username)
     context = UserContext(user)
     return context.detailView(request)
