be313a0dff32e34df5d0162280c023a7062bd95c
Francisco Jair <fjair123@ufpi.edu.br>
2021-05-24 09:20:19 -0300

    fixing name in views file

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,5 +3,5 @@ from .models import ClientUser, PartnerUser
 # Create your views here.
 def detailPartner(request):
     user = UserFacade.getUser(PartnerUser, request.user.username)
-    content = UserContext(user)
+    context = UserContext(user)
     return context.detailView(request)
