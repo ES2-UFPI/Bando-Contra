ebddd2ef0960ab6f0fd551e289928957bb9115eb
abaeta <40252076+abaeta@users.noreply.github.com>
2021-05-28 22:59:38 -0300

    Add modelForm Client #2 (#24)

---
+++ backend/core/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from .models import ClientUser
+
+class ClientUserForm(ModelForm):
+    def __init__(self):
+        super(ClientUserForm, self).__init__()
+        self.fields['email'].required = True
+    
+    class Meta:
+        model = ClientUser
+        fields = ['username', 'first_name', 'second_name', 'email', 'password', 'cpf', 'address', 'phone', 'bornDate']
