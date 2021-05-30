bdc61924f6401b67c8a949cb5d14a7a6a80388ca
abaeta <40252076+abaeta@users.noreply.github.com>
2021-05-30 09:48:53 -0300

    Feature/add client registerview (#27)
    * WIP #3
    
    * Add client registerview #3
    
    * WIP
    
    * WIP
    
    * Add client register view #3
    
    * add client register view #3

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,56 +1,6 @@
-from django.db import models
-from django.core.validators import RegexValidator
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.models import User
-
-from .storage import OverwriteStorage
-STORAGE=OverwriteStorage(location="_private/users/documents")
-
-class ModelField:
-    def createAddress ():
-        return models.CharField("Address", max_length = 100, default = None)
-    
-    def createCpf ():
-        return models.CharField("CPF", max_length = 11, default = None)
-    
-    def createDate (label):
-        return models.DateField(label, default = None)
-    
-    def createPhone ():
-        phone_regex = RegexValidator(regex=r'\(\d{2}\)\d{4,5}-\d{4}', message="Informe um telefone válido. Exemplos: (99)99999-9999; (99)9999-9999")
-        return models.CharField("Phone Number", max_length = 18, default = None, validators = [phone_regex])
-
-    def createNationality ():
-        return models.CharField("Nationality", max_length = 50, default = None)
-
-    def createValidation ():
-        return models.BooleanField("Validation", default = False)
-
-    def createAssessmentSum ():
-        return models.IntegerField ("Assessment Sum", default = 0)
-
-    def createAssessmentCount ():
-        return models.IntegerField ("Assessment Count", default = 0)
-
-    def createObservation ():
-        return models.CharField("Observation", max_length = 5000, default = None)
-
-    def createDocument ():
-        return models.FileField("Document", storage=STORAGE)
-
-class ShortcutsFacade:
-    @staticmethod
-    def callRender(request, template, data={}):
-        return render(request, template, data)
-
-class UserFacade:
-    @staticmethod
-    def addMethodToUser(methodName,method):
-        User.add_to_class(methodName, method)
-
-    @staticmethod
-    def getUser(userModel, username):
-        return get_object_or_404(userModel, username=username)
+from abc import ABC, abstractmethod
+from .forms import ClientUserForm
+from .facade import ShortcutsFacade
 
 class UserContext:
     def __init__(self, user):
@@ -68,3 +18,43 @@ class UserContext:
         result = self._user.getTemplatesLocation() + "detail.html"
         data = {'user': self._user}
         return ShortcutsFacade.callRender(request, result, data)
+
+class UserCreator(ABC):
+    @abstractmethod
+    def factoryMethod(self, request):
+        pass 
+
+    def addUser(self, request, title):
+        
+        if request.method == "POST":
+            user = self.factoryMethod(request)
+            if user != None:
+                user.save()
+                return ShortcutsFacade.callRedirect("detail_client")
+            form = ClientUserForm(request.POST)
+        else:
+            form = self.getForm()
+        data = {'title':title, "form":form}
+        result = self.getTemplatesLocation() + "register.html"
+        return ShortcutsFacade.callRender(request, result, data) 
+    
+    @abstractmethod
+    def getTemplatesLocation(self):
+        pass
+    
+    @abstractmethod
+    def getForm(self):
+        pass
+
+class ClientCreator(UserCreator):
+    
+    def factoryMethod(self, request ):
+        form = ClientUserForm(request.POST)
+        if form.is_valid():
+            return form.save(commit = False)
+
+    def getForm(self):
+        return ClientUserForm()
+    
+    def getTemplatesLocation(self):
+        return "core/user/client/"
