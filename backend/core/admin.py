be2a2d62263b29f5e3c1ca0b99476c5473c42f4c
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-06-14 21:39:35 -0300

    add ModelForm Event #42

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ClientUser, PartnerUser
+from .models import ClientUser, PartnerUser, Event
 
 # Register your models here.
 admin.site.register(ClientUser)
 admin.site.register(PartnerUser)
+admin.site.register(Event)
